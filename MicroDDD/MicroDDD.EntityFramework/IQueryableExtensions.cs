e92091898b37aac19244453dc40a5fa9d68ce2d8
hbsis-fernandobatista <fernando.batista@hbsis.com.br>
2017-10-19 17:24:47 -0200

    QueryableHelper

---
+++ MicroDDD/MicroDDD.EntityFramework/IQueryableExtensions.cs
@@ -0,0 +1,23 @@
+﻿using MicroDDD.Infra.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroDDD.EntityFramework
+{
+    public class QueryableHelper : IQueryableHelper
+    {
+        public Task<T[]> ToArrayAsync<T>(IQueryable<T> consulta)
+        {
+            return consulta.ToArrayAsync();
+        }
+
+        public Task<List<T>> ToListAsync<T>(IQueryable<T> consulta)
+        {
+            return consulta.ToListAsync();
+        }
+    }
+}
---
+++ MicroDDD/MicroDDD.Infra/Helpers/IQueryableHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroDDD.Infra.Helpers
+{
+    public interface IQueryableHelper
+    {
+        Task<List<T>> ToListAsync<T>(IQueryable<T> consulta);
+        Task<T[]> ToArrayAsync<T>(IQueryable<T> consulta);        
+    }
+}
