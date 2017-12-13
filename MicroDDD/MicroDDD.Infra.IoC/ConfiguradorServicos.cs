ece4194750b52e0f04a7d88715c200bbd642c788
fbatist <frbatist@gmail.com>
2017-12-13 08:45:50 -0200

    usings

--- MicroDDD/MicroDDD.Infra.IoC/ConfiguradorServicos.cs
+++ MicroDDD/MicroDDD.Infra.IoC/ConfiguradorServicos.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using MicroDDD.Infra.Helpers;
+﻿using MicroDDD.Infra.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 using MicroDDD.EntityFramework;
 
